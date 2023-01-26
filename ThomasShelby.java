b05aee84a82703451e3fad0c4926394360fa9c06
Nikhil Shashidhar <e0725992@u.nus.edu>
2023-01-26 09:39:40 +0800

    Format indents to A-CodingStandard

--- ThomasShelby.java
+++ ThomasShelby.java
@@ -14,7 +14,9 @@ public class ThomasShelby {
             } else if (cmd.equals("list")) {
                 System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println((i + 1) + ". " + taskManager[i].getStatusIcon() + " " + taskManager[i].getDescription());
+                    System.out.println((i + 1) + ". " 
+                            + taskManager[i].getStatusIcon() + " " 
+                            + taskManager[i].getDescription());
                 }
             } else { // add cmd, mark or unmark
                 String[] cmdSplit = cmd.split("\\s+");
@@ -22,12 +24,14 @@ public class ThomasShelby {
                     int whichTask = Integer.parseInt(cmdSplit[1]) - 1; // shift back index
                     taskManager[whichTask].setIsDone(true);
                     System.out.println("That was long due, well done.");
-                    System.out.println(taskManager[whichTask].getStatusIcon() + " " + taskManager[whichTask].getDescription());
+                    System.out.println(taskManager[whichTask].getStatusIcon() 
+                            + " " + taskManager[whichTask].getDescription());
                 } else if (cmdSplit[0].equals("unmark")) {
                     int whichTask = Integer.parseInt(cmdSplit[1]) - 1; // shift back index
                     taskManager[whichTask].setIsDone(false);
                     System.out.println("You've gotten lazy.");
-                    System.out.println(taskManager[whichTask].getStatusIcon() + " " + taskManager[whichTask].getDescription());
+                    System.out.println(taskManager[whichTask].getStatusIcon() 
+                            + " " + taskManager[whichTask].getDescription());
                 } else { // adding a new task
                     Task newTask = new Task(cmd);
                     newTask.setDescription(cmd);
