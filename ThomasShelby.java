e740362a6bfe2ccf668747d33f784447baea9be1
Nikhil Shashidhar <e0725992@u.nus.edu>
2023-01-28 14:31:23 +0800

    Refactor echo, mark and unmark

--- ThomasShelby.java
+++ ThomasShelby.java
@@ -5,7 +5,7 @@ public class ThomasShelby {
     public static void main(String[] args) {
         Task[] taskManager = new Task[100];
         int taskCount = 0;
-        System.out.print("Good day, I'm Thomas Shelby\nTo what do I owe the pleasure?\n");
+        System.out.print("Good day, I'm Thomas Shelby.\nTo what do I owe the pleasure?\n");
         Scanner in = new Scanner(System.in);
         while (true) {
             String cmd = in.nextLine();
@@ -19,25 +19,27 @@ public class ThomasShelby {
                             + taskManager[i].getDescription());
                 }
             } else { // add cmd, mark or unmark
-                String[] cmdSplit = cmd.split("\\s+");
-                if (cmdSplit[0].equals("mark") && Character.isDigit(cmdSplit[1].charAt(0))) {
+                String[] cmdSplit = cmd.split(" ", 2);
+                if (cmdSplit[0].equals("mark") && Character.isDigit(cmdSplit[1].charAt(0))) { // set tick
                     int whichTask = Integer.parseInt(cmdSplit[1]) - 1; // shift back index
                     taskManager[whichTask].setIsDone(true);
                     System.out.println("That was long due, well done.");
                     System.out.println(taskManager[whichTask].getStatusIcon() 
                             + " " + taskManager[whichTask].getDescription());
-                } else if (cmdSplit[0].equals("unmark")&& Character.isDigit(cmdSplit[1].charAt(0))) {
+                } else if (cmdSplit[0].equals("unmark") && Character.isDigit(cmdSplit[1].charAt(0))) { // set cross
                     int whichTask = Integer.parseInt(cmdSplit[1]) - 1; // shift back index
                     taskManager[whichTask].setIsDone(false);
                     System.out.println("You've gotten lazy.");
                     System.out.println(taskManager[whichTask].getStatusIcon() 
                             + " " + taskManager[whichTask].getDescription());
-                } else { // adding a new task
-                    Task newTask = new Task(cmd);
-                    newTask.setDescription(cmd);
+                } else if (cmdSplit[0].equals("add")) { // add new task
+                    Task newTask = new Task(cmdSplit[1]);
+                    newTask.setDescription(cmdSplit[1]);
                     taskManager[taskCount] = newTask;
                     taskCount++;
                     System.out.print("added: ");
+                    System.out.println(cmdSplit[1]);
+                } else { // echo cmd
                     System.out.println(cmd);
                 }
             }
